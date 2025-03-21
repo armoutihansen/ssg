43cf92251803ecb617fb23300791b61764ecceeb
Jesper Armouti-Hansen <jesper@armoutihansen.xyz>
2025-03-21 20:15:28 +0100

    Adding tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,14 +1,14 @@
 from enum import Enum
 
 class TextType(Enum):
-    BOLD_TEXT = "bold"
-    ITALIC_TEXT = "italic"
-    CODE_TEXT = "code"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
 class TextNode():
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
